c5f73a57148440b5776cf9e8eb4d98aed6a4dbd9
Aleks Boev <aleksb86@gmail.com>
2014-11-27 23:26:10 +0300

    Исправления: допускается ввод слов, содержащих цифры, отменена принудительная проверка типа аргумента.

--- palindrom.py
+++ palindrom.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
 import re
 
 
@@ -14,7 +15,7 @@ def palindrom(raw_str):
     """
     # Очистка входной строки от пунктуации
     # и пробельных символов.
-    my_str = re.sub("[\s.,!-?]", '', raw_str.decode('utf-8'))
+    my_str = re.sub('[\s.:,?!-]', '', raw_str.decode('utf-8'))
 
     for ch_left, ch_right in zip(list(my_str[:len(my_str) / 2]),
                                  list(my_str[::-1][:len(my_str) / 2])):
@@ -27,13 +28,10 @@ def palindrom(raw_str):
 
 if __name__ == '__main__':
     import sys
+    
 
     try:
-        arg_str = sys.argv[1]
-        if not isinstance(arg_str, str):
-            print 'Ошибка типа входных данных! Поддерживаемый тип - строка'
-            sys.exit()
-        print palindrom(arg_str)
+        print palindrom(sys.argv[1])
     except:
-        print 'Ошибка: ', sys.exc_info()[0] 
+        print 'Ошибка: сведения - ', sys.exc_info()[0]
 
